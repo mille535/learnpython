ca60a8cb642e4e9b044ef9206bd156ecf12cd344
Kirk Miller <kirk@kmil.us>
2020-04-17 15:55:47 -0500

    done for the day wk4

--- week4/wk4ex1.py
+++ week4/wk4ex1.py
@@ -12,4 +12,43 @@ Using a for-loop iterate over the dictionary and print out all of the dictionary
 Using a single for-loop iterate over the dictionary and print out all of the dictionary keys and
 values.
 """
+
 from __future__ import unicode_literals, print_function
+
+router1 = {
+    "ip_addr": "172.16.1.1",
+    "vendor": "Cisco",
+    "username": "admin",
+    "password": "monkey123"
+}
+
+print()
+print(router1['ip_addr'])
+print()
+
+if router1['vendor'].lower() == 'cisco':
+    router1['platform'] = 'ios'
+elif router1['vendor'].lower() == 'juniper':
+    router1['platform'] = 'junos'
+
+bgp_fields = {
+    "bgp_as": "1254",
+    "peer_as": "5423",
+    "peer_ip": "172.16.1.2"
+}
+
+router1.update(bgp_fields)
+
+print('-' * 80)
+for i in router1:
+    print("{:>15}".format(i))
+
+print('-' * 80)
+print()
+
+print('-' * 80)
+for key, value in router1.items():
+    print("{key:>15} ---> {value:>15}".format(key=key, value=value))
+
+print('-' * 80)
+print()
